29d33c3e18792ddd8c5b1b856c0fe43ba48dd382
Zhengjian Kang <zk299@nyu.edu>
2023-06-25 22:41:05 -0400

    Create dataStructure.cpp

---
+++ dataStructure.cpp
@@ -0,0 +1,15 @@
+// array
+
+
+
+// hash map
+unordered_map<string,vector<string>> map1; // default value to empty vector<stinrg>?
+for (auto a : map1) {
+  results.push_back(a.second);
+}
+unordered_map<string, int> map2; // default value to 0?
+
+
+
+
+

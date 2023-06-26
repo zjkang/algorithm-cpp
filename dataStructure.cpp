d25a7218c46dd1623076c4b1e0d7f7e1cdf35026
Zhengjian Kang <zk299@nyu.edu>
2023-06-25 22:49:04 -0400

    Update dataStructure.cpp

--- dataStructure.cpp
+++ dataStructure.cpp
@@ -2,12 +2,16 @@
 
 
 
+// -----------------------------------------------------------
 // hash map
+// https://cplusplus.com/reference/unordered_map/unordered_map/
 unordered_map<string,vector<string>> map1; // default value to empty vector<stinrg>?
-for (auto a : map1) {
+for (auto a : map1) { // key -> a.first, value -> a.second
   results.push_back(a.second);
 }
 unordered_map<string, int> map2; // default value to 0?
+
+
 
 
 

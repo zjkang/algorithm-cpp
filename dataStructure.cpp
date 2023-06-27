5ebe091a5aec705674c3bc06948f51442c0e04b4
Zhengjian Kang <zk299@nyu.edu>
2023-06-26 20:06:53 -0400

    Update dataStructure.cpp

--- dataStructure.cpp
+++ dataStructure.cpp
@@ -1,6 +1,24 @@
 // array
 
 // vector
+// p1
+vector<int> vec = {1,2,3};
+int sum = 0;
+for (auto val : vec) {
+  sum += val;
+}
+// p2
+vector<int> vec = {1,2,3};
+int sum = 0;
+for (const auto& val : vec) {
+  sum += val;
+}
+// p3
+vector<int> vec = {1,2,3};
+int sum = std:accumulate(vec.begin(), vec.end(), 0);
+
+
+
 
 // list
 

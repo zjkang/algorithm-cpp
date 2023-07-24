356e5454e8d361d0670d87a9dc633b9c3dc3e539
Zhengjian Kang <zk299@nyu.edu>
2023-07-23 21:08:25 -0400

    Update others.cpp

--- data_structure/others.cpp
+++ data_structure/others.cpp
@@ -1,10 +1,8 @@
-```c++
 // null pointer
 int* a = NULL; // or prefer to use 
 int* b = nullptr;
-```
 
-```c++
+
 // long name type usage
 typedef long long LL;
 typedef pair<LL, LL> PLL;
@@ -12,11 +10,8 @@ LL a = 0;
 
 // modern c++ 
 using LL = long long;
-```
 
 
-
-```c++
 // apply lambda function (https://en.cppreference.com/w/cpp/language/lambda)
 // format: [captures](parameters) {body}
 // e.g.,
@@ -25,9 +20,7 @@ static bool cmp(vector<int>&a, vector<int>&b) {
 }
 // < == >
 [](auto& a, auto& b) { return a[1] < b[1]; }
-```
 
-```c++
- // pair<> type
- pair<int, int> pair = make_pair(1, 2);
-```
+
+// pair<> type
+pair<int, int> pair = make_pair(1, 2);

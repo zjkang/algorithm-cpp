2a70f4324bb55d203dc14bc6e3af2f28c93ff544
Zhengjian Kang <zk299@nyu.edu>
2023-07-23 21:09:45 -0400

    Create iterator.cpp

---
+++ data_structure/iterator.cpp
@@ -0,0 +1,13 @@
+// https://cplusplus.com/reference/iterator/
+
+// Input (Read)    <-- Forward   <-- Bidirectional  <-- Random Access
+
+// Output (Write)
+
+// begin, end, prev, next, back_inserter
+int foo[] = {10,20,30,40,50};
+std::vector<int> bar;
+// iterate foo: inserting into bar
+for (auto it = std::begin(foo); it!=std::end(foo); ++it) {
+    bar.push_back(*it);
+}

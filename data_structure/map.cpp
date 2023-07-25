8e5e45aa2190d2b3527eba8583e25743c2974e12
Zhengjian Kang <zk299@nyu.edu>
2023-07-25 00:06:27 -0400

    Create map.cpp

---
+++ data_structure/map.cpp
@@ -0,0 +1,28 @@
+// hash map
+
+// https://cplusplus.com/reference/unordered_map/unordered_map/
+
+// default value to empty vector<stinrg>
+unordered_map<string,vector<string>> map1; 
+// default value to 0
+unordered_map<string, int> map2; 
+
+// key -> a.first, value -> a.second
+for (auto a : map1) { 
+  results.push_back(a.second);
+}
+
+map1["a"] = {"1","2"};
+if (map1.find("a")!=map1.end()) {
+}
+map1.count("a");
+
+
+// ----------------------------------------------------------
+// tree map
+
+// https://en.cppreference.com/w/cpp/container/map
+// https://en.cppreference.com/w/cpp/container/map/lower_bound
+
+map<int, int> map;
+auto lower_iter = map.lower_bound(1);

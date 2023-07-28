e571c234cf79557f83279b0e398bc95f17a13e9e
Zhengjian Kang <zk299@nyu.edu>
2023-07-28 16:23:34 -0400

    Update containers.cpp

--- stanford_c++/containers.cpp
+++ stanford_c++/containers.cpp
@@ -5,6 +5,7 @@
 // - Algorithms
 
 
+// -----------------------------------------
 // Sequence Containers: sequences of elements
 std::vector<T>
 std::deque<T>
@@ -14,14 +15,67 @@ std::forward_list<T>
 
 // standard C++ version
 vector<int> v = {1, 3, 7};
+v.push_back(271);
+v.front();
+v.back();
+v.erase(v.begin());
+v.pop_back();
+// no such thing as a sublist
+
+// loop
+for (size_t i = 0; i < v.size(); ++i) {
+    cout << v[i] << endl;
+}
+for (string elem : v) {
+    cout << elem << endl;
+}
+
+v.push_front(); // can be very slow due to moving the all the elements
 
 
+// note: vector outperforms a deque for element access. 
+deque<int> d = {1, 3, 7};
+d.push_back(271);
+d.push_front(-1);
+d.front();
+d.back();
+
+
+// -----------------------------------------
 // adaptors
+// stack & queue are container adaptors
+
+template<
+  class T,
+  class Container = std::deque<T>
+> class stack;
+// provide functions: back(), push_back(), pop_back()
+
+template<
+  class T,
+  class Container = std::deque<T>
+> class queue;
+// provide functions: back(), front(), push_back(), pop_front()
+
 
 // iterators
 
 // functors
 
-// algorithms
+
+// -----------------------------------------
+// stl algorithms
+const int kNumInts = 200;
+std::vector<int> vec(kNumInts);
+std::generate(vec.begin(), vec.end(), rand);
+std::sort(vec.begin(), vec.end());
+std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(count, "\n"));
+
+
+
+
+
+
+
 
 

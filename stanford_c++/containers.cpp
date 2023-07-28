ebe933ea75b975f7abbb6c4761dac9099ecf6479
Zhengjian Kang <zk299@nyu.edu>
2023-07-28 15:03:11 -0400

    Update containers.cpp

--- stanford_c++/containers.cpp
+++ stanford_c++/containers.cpp
@@ -1,8 +1,19 @@
-// containers
-// Sequence Containers: sequences of elements
-// 
+// Overview of STL
+// - Containers (include Adaptors)
+// - Iterators
+// - Functors
+// - Algorithms
 
+
+// Sequence Containers: sequences of elements
 std::vector<T>
+std::deque<T>
+std::list<T>
+std::array<T>
+std::forward_list<T>
+
+// standard C++ version
+vector<int> v = {1, 3, 7};
 
 
 // adaptors

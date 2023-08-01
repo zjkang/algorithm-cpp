73447955cec2babf166579c136b251a95984b78c
Zhengjian Kang <zk299@nyu.edu>
2023-07-31 23:01:21 -0400

    Update containers.cpp

--- stanford_c++/containers.cpp
+++ stanford_c++/containers.cpp
@@ -270,6 +270,27 @@ int val2 = *itr;
 // - Pointers
 
 
+// erase in STL containers
+
+// compare with vector, deque and list for different iterator types
+vector<int> v{3, 1, 4, 1, 5, 2, 6};
+auto iter = v.begin();
+std::advance(iter, 4); // could also do iter += 4
+v.erase(iter); // cannot erase by index
+
+
+
+
+
+
+
+
+
+// -----------------------------------------
+// templates
+
+
+
 // -----------------------------------------
 // functors
 

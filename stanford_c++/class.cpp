a220efe093f231d9b80c70c506b1aa204fad106a
Zhengjian Kang <zk299@nyu.edu>
2023-08-28 15:51:32 -0400

    Create class.cpp

---
+++ stanford_c++/class.cpp
@@ -0,0 +1,19 @@
+Constructors
+
+// Uniform Initialization
+Student::Student() : name{“”}, age{0}, state{“”} {}
+Student::Student(string name, int age, string state) : name{name}, age{name}, state{state} {}
+
+
+Destructors
+
+
+Template Class
+template<typename First, typename Second>
+struct MyPair { 
+  First first;
+  Second second;
+};
+
+Templates don’t emit code until instantiated, so include the .cpp in the .h instead of the other way around!
+

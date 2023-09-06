a591cc8948626601d571298ef4d2c9863ef32dc1
Zhengjian Kang <zk299@nyu.edu>
2023-09-05 22:22:07 -0400

    Update priorityQueue.cpp

--- data_structure/priorityQueue.cpp
+++ data_structure/priorityQueue.cpp
@@ -6,7 +6,7 @@
 //     class Container = std::vector<T>,
 //     class Compare = std::less<typename Container::value_type>
 // > class priority_queue;
-const auto data = {1, 8, 5, 6, 3, 4, 0, 9, 7, 2};
+vector<int> data = {1, 8, 5, 6, 3, 4, 0, 9, 7, 2};
 print("data", data);
 std::priority_queue<int> q1; // Max priority queue
 for (int n : data) {
@@ -15,36 +15,30 @@ for (int n : data) {
 
 
 // Min priority queue
-// std::greater<int> makes the max priority queue act as a min priority queue
+// 1. std::greater<int> makes the max priority queue act as a min priority queue
 std::priority_queue<int, std::vector<int>, std::greater<int>> minq1(data.begin(), data.end());
-// Second way to define a min priority queue
+
+// 2. define a min priority queue
 std::priority_queue minq2(data.begin(), data.end(), std::greater<int>());
 
-
-// Using a custom function object to compare elements.
-struct
-{
+// 3. Using a custom function object to compare elements.
+struct {
     bool operator()(const int l, const int r) const { return l > r; }
 } customLess;
-std::priority_queue minq3(data.begin(), data.end(), customLess); // Min priority queue
+std::priority_queue minq3(data.begin(), data.end(), customLess); 
 
+// 4. Using a custom function object to compare elements.
+struct CustomLess {
+    bool operator()(const int l, const int r) const { return l > r; }
+};
+std::priority_queue<int, vector<int>, CustomLess> minq4(data.begin(), data.end());
 
-// Using lambda to compare elements.
+// 5. Using lambda to compare elements.
 auto cmp = [](int left, int right) { return left < right; };
 std::priority_queue<int, std::vector<int>, decltype(cmp)> q5(cmp); // Max priority queue
 for (int n : data) {
     q5.push(n);
 }
-
-// result
-// data:   1 8 5 6 3 4 0 9 7 2
-// q1:     9 8 7 6 5 4 3 2 1 0
-// minq1:  0 1 2 3 4 5 6 7 8 9
-// minq2:  0 1 2 3 4 5 6 7 8 9
-// minq3:  0 1 2 3 4 5 6 7 8 9
-// q5:     9 8 7 6 5 4 3 2 1 0
-
-
 
 // APIs
 pq.top(); 

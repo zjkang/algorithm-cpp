a0d9c2a552d223e1d669e40378f95ca38b57a5a1
Zhengjian Kang <zk299@nyu.edu>
2023-09-06 16:35:26 -0400

    Update _priorityQueue.cpp

--- data_structure/_priorityQueue.cpp
+++ data_structure/_priorityQueue.cpp
@@ -48,4 +48,34 @@ pq.push();
 pq.pop();
 
 
+// constructs element <in-place> and sorts the underlying container
+pq.emplace();
+
+// e.g.
+#include <iostream>
+#include <queue>
+ 
+struct S
+{
+    int id;
+ 
+    S(int i, double d, std::string s) : id{i}
+    {
+        std::cout << "S::S(" << i << ", " << d << ", \"" << s << "\");\n";
+    }
+ 
+    friend bool operator< (S const& x, S const& y) { return x.id < y.id; }
+};
+ 
+int main()
+{
+    std::priority_queue<S> adaptor;
+ 
+    adaptor.emplace(42, 3.14, "C++");
+ 
+    std::cout << "id: " << adaptor.top().id << '\n';
+}
+
+
+// ----------------------------------------------
 

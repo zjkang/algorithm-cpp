017c4fe71996836d54e051cb2d5bbd34954bcfaf
Zhengjian Kang <zk299@nyu.edu>
2023-09-06 20:46:34 -0400

    Update others.cpp

--- data_structure/others.cpp
+++ data_structure/others.cpp
@@ -29,5 +29,34 @@ pair<int, int> pair = {1,2};
 
 
 // modern c++
+// initializer list
+// ex 1
+vector<int> v{1,2,3,5}; // <==> initializer_list<int> l = {1,2,3,5}; vector<int> v(l);
+vector<int> v = {1,2,3,5}; // assignment
+vector<int> v{1,2,3,5}; // construction
+// ex 2
+vector<pair<int, float>> v{{1,2.5f}, {3,3.14f}};
+// ex 3
+map<string, int>> m{{"hello", 0}, {"world", 1}};
+
+// uniform initialization
+int x = 2; => int x(2); => int x{2};
+pair<int, int> x(3,4); // constructor {3,4}
+pair<int, int> x{3,4}; // {3,4}
+vector<int> v(5,1); // v = [1,1,1,1,1]
+vector<int> v{5,1}; // v = {5,1};
+
+vector<int> foo() {
+    set<int> ans = ...
+    return vector<Mint>(begin(ans), end(ans));
+    <==>
+    return {begin(ans), end(ans)};
+}
+
+
+
+
+
+
 
 

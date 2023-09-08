af1a92646aa13ad648c8c9d207052ac834120ef9
Zhengjian Kang <zk299@nyu.edu>
2023-09-08 15:01:34 -0400

    Create _list.cpp

---
+++ data_structure/_list.cpp
@@ -0,0 +1,37 @@
+// https://en.cppreference.com/w/cpp/container/list
+template<
+    class T,
+    class Allocator = std::allocator<T>
+> class list;
+
+list<int> l;
+l.push_back(1); l.push_front(1);
+l.pop_back(); l.pop_front();
+l.empty(); l.size();
+l.front(); l.back();
+
+#include <algorithm>
+#include <iostream>
+#include <list>
+ 
+int main()
+{
+    // Create a list containing integers
+    std::list<int> l = {7, 5, 16, 8};
+ 
+    // Add an integer to the front of the list
+    l.push_front(25);
+    // Add an integer to the back of the list
+    l.push_back(13);
+ 
+    // Insert an integer before 16 by searching
+    auto it = std::find(l.begin(), l.end(), 16);
+    if (it != l.end())
+        l.insert(it, 42);
+ 
+    // Print out the list
+    std::cout << "l = { ";
+    for (int n : l)
+        std::cout << n << ", ";
+    std::cout << "};\n";
+}

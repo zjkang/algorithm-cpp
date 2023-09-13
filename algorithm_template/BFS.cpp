8078361d7270ec2ca400a0b3e89897a006cb0df0
Zhengjian Kang <zk299@nyu.edu>
2023-09-13 09:00:50 -0400

    Update BFS.cpp

--- algorithm_template/BFS.cpp
+++ algorithm_template/BFS.cpp
@@ -10,8 +10,8 @@
 
 // Q1.1 BFS Template w/o Level Traversal
 // 无需分层遍历的宽度优先
-queue<int> queue;
-unordered_set<int> seen;
+queue<Node*> queue;
+unordered_set<Node*> seen;
 Node* start = new Node();
 queue.push(start);
 seen.insert(start);
@@ -48,10 +48,60 @@ while (!queue.empty()) {
   }
 }
 
+
+// Q1.3 BFS Template w/ Bi-Directional Traversal
+// bi-directional bfs
+void doubleBFS(Node* start, Node* end) {
+  if (start == end) return 1;
+  // 分别从起点和终点开始的两个BFS队列
+  queue<Node*> startQueue, endQueue;
+  startQueue.push(start);
+  endQueue.push(end);
+  
+  int step = 0;
+  // 从起点开始和从终点开始分别访问过的节点集合
+  set<Node*> startVisited, endVisited;
+  startVisited.insert(start);
+  endVisited.insert(end);
+
+  while (startQueue.size() && endQueue.size()) {
+    int startSize = startQueue.size();
+    int endSize = endQueue.size();
+    //　按层遍历
+    step++;
+    for (int i = 0; i < startSize; i++) {
+      auto cur = startQueue.top();
+      startQueue.pop();
+      for (auto neighbor : cur->neighbors) {
+        if (startVisited.count(neighbor)) continue;
+        else if (endVisited.count(neighbor)) return step;
+        else {
+          startVisited.insert(neighbor);
+          startQueue.push(neighbor);
+        }
+      }
+    }
+
+    step++;
+    for (int i = 0; i < endSize; i++) {
+      auto cur = endQueue.top();
+      endQueue.pop();
+      for (auto neighbor : cur->neighbors) {
+        if (endVisited.count(neighbor)) continue;
+        else if (startVisited.count(neighbor)) return step;
+        else {
+          endVisited.insert(neighbor);
+          endQueue.push(neighbor);
+        }
+      }
+    }
+  }
+  return -1;
+}
+
+
 // https://leetcode.com/problems/minimum-depth-of-binary-tree/
 // https://leetcode.com/problems/is-graph-bipartite/
 // https://www.lintcode.com/problem/clone-graph/description
 // https://leetcode.com/problems/open-the-lock/
 // https://leetcode.com/problems/the-maze/
-
-

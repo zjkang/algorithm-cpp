b8c225b46a4f68378cb0b935605084ad5a195a03
zjkang <zk299@nyu.edu>
2023-11-10 00:15:07 -0500

    refactor

---
+++ data_structure/_most_common.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+// most common c++ operations
+int main() {
+//   cout << "-------- VECTOR ----------- " << endl;
+//   int a[5] = {1,2,3,4,5};
+//   vector<int> b{a, a+5};
+//   b.push_back(6);
+//   b.pop_back();
+//   cout << b.at(1) << "," << b.back() << "," << b.front() << endl;
+//   sort(b.begin(), b.end());
+//   sort(b.rbegin(), b.rend());
+//   cout << b.at(1) << "," << b.back() << "," << b.front() << endl;
+//   int min_val = *min_element(b.begin(), b.end());
+//   cout << min_val << endl;
+  
+  
+  // cout << "-------- MAP ----------- " << endl;
+  // map<int, int> c_map = {{2,1},{3,4},{1,2}};
+  // for (auto kv : c_map) cout << kv.first << "," << kv.second << endl;
+  // c_map[10] = 1;
+  // auto it = c_map.erase(++c_map.begin()); // remove key 2
+  // cout << it->first << endl; // next point to key 3
+  // cout << endl;
+  // map<int,int, greater<int>> d_map = {{2,1},{3,4},{1,2}};
+  // for (auto kv : d_map) cout << kv.first << "," << kv.second << endl;
+  // cout << endl;
+  
+  
+//   cout << "-------- Priority Queue ----------- " << endl;
+//   priority_queue<int, vector<int>> pq; // max-heap
+//   pq.push(1); pq.push(3); pq.push(10); pq.push(5);
+//   while (!pq.empty()) {
+//     cout << pq.top() << endl;
+//     pq.pop();
+//   }
+//   cout << endl;
+//   priority_queue<int, vector<int>, greater<int>> pq2; // min-heap
+//   pq2.push(1); pq2.push(3); pq2.push(10); pq2.push(5);
+//   while (!pq2.empty()) {
+//     cout << pq2.top() << endl;
+//     pq2.pop();
+//   }
+
+  return 0;
+}

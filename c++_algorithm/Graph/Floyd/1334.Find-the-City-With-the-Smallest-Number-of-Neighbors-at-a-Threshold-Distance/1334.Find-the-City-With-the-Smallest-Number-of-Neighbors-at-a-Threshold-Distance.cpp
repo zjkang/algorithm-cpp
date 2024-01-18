71aa80fdce8a80c829965a726089b14c2648cf2c
zjkang <zk299@nyu.edu>
2024-01-18 15:39:13 -0500

    update floyd

--- c++_algorithm/Graph/Floyd/1334.Find-the-City-With-the-Smallest-Number-of-Neighbors-at-a-Threshold-Distance/1334.Find-the-City-With-the-Smallest-Number-of-Neighbors-at-a-Threshold-Distance.cpp
+++ c++_algorithm/Graph/Floyd/1334.Find-the-City-With-the-Smallest-Number-of-Neighbors-at-a-Threshold-Distance/1334.Find-the-City-With-the-Smallest-Number-of-Neighbors-at-a-Threshold-Distance.cpp
@@ -2,6 +2,7 @@
 // 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance
 
 // Floyd算法的直接应用
+// dp[i][j]: the shortest distance between node i and j
 
 
 class Solution {
--- c++_algorithm/Graph/Floyd/readme.md
+++ c++_algorithm/Graph/Floyd/readme.md
@@ -1,4 +1,6 @@
+### Floyd算法
+
 Floyd算法用来计算所有源点之间的最短距离。时间复杂度O(n^3), n是图中顶点的数量。
 
-dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j])
+`dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j])`
 
